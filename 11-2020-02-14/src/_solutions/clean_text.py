6c9743c9565f7449cd80c76bb2dd7d6fd9d85feb
eotp <krois@zedat.fu-berlin.de>
2020-02-14 02:52:18 +0100

    add class 11

---
+++ 11-2020-02-14/src/_solutions/clean_text.py
@@ -0,0 +1,16 @@
+def clean_text(l, pattern):
+    rv = []
+    for s in l:
+        for p in pattern:
+            s = s.replace(p, ' ')
+        s = s.strip()
+        if s.startswith('Anmeldung'):
+            pass
+        else:
+            rv.append(s)
+    try:
+        return rv[0]
+    except:
+        print(f'An exception occured!\n')
+        pass
+clean_text(txt, ['\n', 'Schließen', '\r'])
---
+++ 11-2020-02-14/src/_solutions/text_exctraction.py
@@ -0,0 +1,9 @@
+def text_exctraction(link):
+    # access the website
+    page = requests.get(link)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    # extract the html blocks
+    info_obj = soup.find_all('div', {"class": 'opened_info'})
+    # extract the text data
+    txt = [obj.text for obj in info_obj]
+    return txt
