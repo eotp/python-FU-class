cbec070d0705b09f1e17551592bbb0ef4d22fb54
krasowskialeks <aleksander.krasowski@ixians.com>
2021-02-17 14:14:47 +0100

    API lecture (unfinished)

---
+++ 14-2021-02-19/src/main.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+class Item(BaseModel):
+    name: str
+    price: float
+    is_offer: bool = None
+
+@app.get("/")
+async def read_root():
+    return {"message": "Hello World"}
+
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: int, q: str = None):
+    return {"item_id": item_id, "q": q}
+
+@app.put("/items/{item_id}")
+async def create_item(item_id: int, item: Item):
+    return {"item_name": item.name, "item_id": item_id}
