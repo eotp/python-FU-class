f46e812b7d127f69dbf55aa9817e72a049c75d8a
krasoa <a.krasowski@fu-berlin.de>
2021-07-08 22:40:54 +0200

    remove duplicate webscraping + move API lecture

--- 13-2021-xx-xx/src/_solutions/myname.py
+++
@@ -1,5 +0,0 @@
-class MyName:
-    name = 'Mikey Mouse'
-
-    def print_name(self):
-        print(self.name)
--- 13-2021-xx-xx/src/_solutions/text_extraction.py
+++
@@ -1,9 +0,0 @@
-def text_extraction(link):
-    # access the website
-    page = requests.get(link)
-    soup = BeautifulSoup(page.content, 'html.parser')
-    # extract the html blocks
-    info_obj = soup.find_all('div', {"class": 'opened_info'})
-    # extract the text data
-    txt = [obj.text for obj in info_obj]
-    return txt
