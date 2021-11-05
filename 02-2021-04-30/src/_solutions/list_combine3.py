53b397dd49f2268c10cd8d8f4bda572539c25a28
Aleksander Krasowski <aleksander.krasowski@ixians.com>
2021-11-05 09:10:38 +0100

    change date

--- 02-2021-04-30/src/_solutions/list_combine3.py
+++
@@ -1,4 +0,0 @@
-
-# Solution 3
-# using a list comprehension
-[f'{x} {y}' for x,y in zip(name, surname)]
--- 02-2021-04-30/src/_solutions/sumlist.py
+++
@@ -1,7 +0,0 @@
-numlist = [1, 4, 77, 3]
-
-result = 0
-for value in numlist:
-    result = result + value
-    
-print(result)
