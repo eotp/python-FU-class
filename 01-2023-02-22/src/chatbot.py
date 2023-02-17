c07cff1714e7dd4960446e6c67ea552b4b761878
eotp <krois@zedat.fu-berlin.de>
2023-02-17 18:31:09 +0100

    add jan to chatbot

--- 01-2023-02-22/src/chatbot.py
+++ 01-2023-02-22/src/chatbot.py
@@ -7,7 +7,7 @@ user = input()
 if user.lower() == "joachim":
     print("Good afternoon my creator!")
     print("")
-elif user.lower() in ["alex", "leo", "martha"]:
+elif user.lower() in ["jan"]:
     print("Oh, what a pleasure! It feels great to have you in the house.")
     time.sleep(1)
     print("Is Joachim somewhere around too?")
